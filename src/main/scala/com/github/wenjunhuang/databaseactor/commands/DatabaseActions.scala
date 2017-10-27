c3d6bd2a58541a879a91d816c58f5c2a78231d89
黄文俊 <huangwj@goldlokedu.cn>
2017-10-27 13:40:42 +0800

    优化

--- src/main/scala/com/github/wenjunhuang/databaseactor/commands/DatabaseActions.scala
+++ src/main/scala/com/github/wenjunhuang/databaseactor/commands/DatabaseActions.scala
@@ -9,8 +9,4 @@ case class DatabaseAction[T](fn: DSLContext => T)
 
 case class DatabaseActionWithPromise[T](promise: Promise[T], fn: DSLContext ⇒ T)
 
-object DatabaseActionWithPromise {
-  def apply[T](promise: Promise[T])(fn: DSLContext ⇒ T) = new DatabaseActionWithPromise(promise, fn)
-}
-
 case class DatabaseActionResult[T](val result: Try[T])
